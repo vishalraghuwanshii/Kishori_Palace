10abec14da0c7d86ec2846ffd0ddd8aec554e29b
Vishal Raghuwanshi <itsvishalraghuwanshi@gmail.com>
2026-07-25 00:56:25 +0530

    chore: optimize mobile layouts, fix horizontal overflow, and update contact number

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} ${playfair.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col overflow-x-hidden">
         <Navbar />
         <main className="flex-grow">
           {children}
